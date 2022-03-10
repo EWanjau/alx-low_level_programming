0fa521d8a0408da2370a0ae602466046e0915b68
EWanjau <wamwanjau@gmail.com>
2022-03-10 12:55:33 -0800

    printing without printf and puts functions

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -5,9 +5,10 @@
  *
  * Return: Always 0 (Success)
  */
+
 int main(void)
 {
-	char *s="and that piece of art is useful\" - Dora Korpar, 2015-10-19\n"; 
-	while (*s) putchar(*s++);
-	return (1);
+char *s = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n"; 
+while (*s) putchar(*s++);
+return (1);
 }
