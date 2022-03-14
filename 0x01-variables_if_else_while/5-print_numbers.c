41520cc21b275d058050f5dbf662635b97152c28
EWanjau <33689244+EWanjau@users.noreply.github.com>
2022-03-14 18:12:47 +0300

    printing numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+*main - Entry Point
+*
+*Return: Always 0 (Success)
+*/
+
+int main(void){
+	int num;
+	num = 0;
+	while(num >= 0 && num < 10)
+		printf('%d',num);
+
+	return(0);
+}
