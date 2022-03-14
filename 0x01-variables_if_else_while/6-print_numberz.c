e85e35a17b7ae9de5fb49cab174a2f7f7d38b93a
EWanjau <33689244+EWanjau@users.noreply.github.com>
2022-03-14 18:13:57 +0300

    printing numbers of base 10

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+*main - Entry point
+*
+*Return: Always 0 (Success)
+*/
+
+int main(void){
+	int num;
+	num = 0;
+	while(num >= 0 && num < 10)
+		putchar(num);
+	return(0);
+}
