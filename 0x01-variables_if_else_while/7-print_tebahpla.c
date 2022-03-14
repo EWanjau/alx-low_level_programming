2ef7096df5b72cee0d413ea708592f6027d00364
EWanjau <33689244+EWanjau@users.noreply.github.com>
2022-03-14 18:15:08 +0300

    print alphabet in reverse

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+*main - Entry Point
+*
+*Return: Always 0 (Success)
+*/
+
+int main(void){
+	char ch;
+	for(ch == 'z';ch >= 'a'; ch--)
+		purchar(ch);
+	return(0);
+}
