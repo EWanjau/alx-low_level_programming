d184e15da2b1f37f63a3c09c2b04659e845da86f
EWanjau <33689244+EWanjau@users.noreply.github.com>
2022-03-14 18:08:54 +0300

    printing an alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void){
+	char ch;
+	for (ch == 'a'; ch <= 'z'; ch++){
+		putchar(char);
+	}
+return (0);
+}
