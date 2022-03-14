d6d30dccafbcfca6c95a7d53f08ed1cac5c63556
EWanjau <33689244+EWanjau@users.noreply.github.com>
2022-03-14 18:57:04 +0300

    print alphabet in lowercase

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -6,10 +6,10 @@
  *
  * Return: Always 0 (Success)
  */
-int main(void){
+int main(void)
+{
 	char ch;
-	for (ch == 'a'; ch <= 'z'; ch++){
-		putchar(char);
-	}
-return (0);
+	for (ch='a';ch<='z';ch++)
+		putchar(ch);
+        return (0);
 }
