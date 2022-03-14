4e1e36367ad1ea4589c68410c4eead83918bfe19
EWanjau <33689244+EWanjau@users.noreply.github.com>
2022-03-14 18:11:54 +0300

    print all letters except two

---
+++ 0x01-variables_if_else_while/4-print_aphabt.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+*main - Entry Point
+*
+*Return: Always 0 (Success)
+*/
+int main(void){
+	char ch;
+	for(ch == 'a'; ch < 'e' && ch > 'e' && ch < 'q' && ch > 'q'; ch++)
+		putchar(ch);
+	return(0);
+}
