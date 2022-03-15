90888253d2966f5501e7e520e423235c54ce7e38
EWanjau <wamwanjau@gmail.com>
2022-03-15 13:04:17 -0700

    absolute no

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * abs(int)- calculates the absolute vaue
+ *
+ * Return: Alwas 0 (Success)
+ */
+int _abs(int)
+{
+	int n;
+	if (n < 0)
+		return (-n);
+	else
+		return (n);	
+}
