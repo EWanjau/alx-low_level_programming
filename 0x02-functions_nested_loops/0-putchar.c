95565e8cbe95bb0105399dfcc26a6dfb3d5bfde1
EWanjau <wamwanjau@gmail.com>
2022-03-15 09:02:08 -0700

    the ptchar function

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -6,14 +6,16 @@
  * Return: Always 0 (Success)
  */
 
-int main()
+int main(void)
 {
-	char str[10]="_putchar";
-	int i =0;
-	while(str[i]!='\0')
+	char str[10] = "_putchar";
+	int i = 0;
+
+	while (str[i] != '\0')
 	{
-		putchar(str[i]);
+		putchar (str[i]);
 		i++;
 	}
+	putchar ("\n");
 	return (0);
 }
