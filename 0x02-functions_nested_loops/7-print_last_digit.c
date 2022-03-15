f7e065ff50bafbd5c7f70b442364e2864338cc93
EWanjau <wamwanjau@gmail.com>
2022-03-15 13:15:56 -0700

    the last digit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * print_last_digit - gets the last digit of am integer
+ *@r:is the single integer
+ * Return: the value of the last digit
+ */
+int print_last_digit(int r)
+{
+	int last = r % 10;
+	return (last);
+}
