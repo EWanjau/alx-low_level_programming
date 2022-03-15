d15a7e4e36b7419324e2c56798040da18c9866fa
EWanjau <33689244+EWanjau@users.noreply.github.com>
2022-03-15 21:46:03 +0300

    print character

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -8,13 +8,15 @@
 
 int main(void)
 {
-	char str[10] = "_putchar ";
-	int i = 0;
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
 
-	while (str[i] != '\0')
-	{
-		_putchar (str[i]);
-		i++;
-	}
 	return (0);
 }
