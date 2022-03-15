046421c5af7559737d402aa42ac0277567ede204
EWanjau <33689244+EWanjau@users.noreply.github.com>
2022-03-15 22:52:34 +0300

    sign checker

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -6,7 +6,7 @@
  * Return: 1 for +, 0 f0r 0 and -1 for -1
  */
 
-int print_sign (int n)
+int print_sign(int n)
 {
 	if (n > 0)
 	{
@@ -14,11 +14,11 @@ int print_sign (int n)
 		return (1);
 	}
 	else if (n == 0)
-	{     
-	       	_putchar('0');
+	{
+		_putchar('0');
 		return (0);
 	}
-	else 
+	else
 	{
 		_putchar('-');
 		return (-1);
