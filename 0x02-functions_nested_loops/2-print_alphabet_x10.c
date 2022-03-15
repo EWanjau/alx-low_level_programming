3d9c9a0532736445c3280a996592c8400327d639
EWanjau <33689244+EWanjau@users.noreply.github.com>
2022-03-15 22:11:29 +0300

    print alpha

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,14 +1,15 @@
 #include "main.h"
 
-/** 
+/**
  * print_alphabet - program prints the alphabet 10 times
  *
- * Return: Always 0 (Success) 
+ * Return: Always 0 (Success)
  */
 
 void print_alphabet_x10(void)
 {
 	int i,j;
+	
 	i = 0;
 
 	while (i <= 10)
