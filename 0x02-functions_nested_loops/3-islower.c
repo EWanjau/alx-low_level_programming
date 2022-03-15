dfd26a36d92b4289624bc4e7952b1f2dc97709d1
EWanjau <33689244+EWanjau@users.noreply.github.com>
2022-03-15 22:21:14 +0300

    check lower

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -2,8 +2,8 @@
 
 /**
  * _islower - Program that checks the input for lowercase
- *
- * Return: Always 0 (Success)
+ * c: single letter
+ * Return: 1 if int c is lowercase, 0 if otherwise
  */
 int _islower(int c)
 {
