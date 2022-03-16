0fcbdf4fefe1a0be55ceb7f37870fdb93b7fb353
EWanjau <wamwanjau@gmail.com>
2022-03-16 13:21:40 -0700

    9 times table

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,43 @@
+#include "main.h"
+
+/**
+ * times_table - 9 times table
+ *
+ * Return: Always 0.
+ */
+void times_table(void)
+{
+	int a, b, c;
+	
+	a = 0;
+	while (a < 10)
+	{
+		b = 0;
+
+		while (b < 10)
+		{
+			c = a * b;
+			if (b == 0)
+			{
+				_putchar(c + '0');
+			}
+			if (c < 10 && b != 0)
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar(' ');
+				_putchar(k + '0');
+			} else if (c >= 10)
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar((k / 10) + '0');
+				_putchar((k % 10) + '0');
+			}
+		b++;
+		}
+	_putchar('\n');
+	a++;
+	}
+
+}
