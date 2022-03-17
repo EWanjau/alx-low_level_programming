df908c1a438267a119f49902eda5c1e7ac0e197e
EWanjau <33689244+EWanjau@users.noreply.github.com>
2022-03-18 00:29:04 +0300

    largest of three

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -19,11 +19,11 @@ int largest_number(int a, int b, int c)
 	else if (c < 0)
 		c = (-1 * c);*/
 
-	if (a >= b && b >= c)
+	if (a > b && b > c)
 	{
 		largest = a;
 	}
-	else if (b >= a && a >= c)
+	else if (b > a && a > c)
 	{
 		largest = b;
 	}
