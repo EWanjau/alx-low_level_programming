53865740711da4566786b1ea29ab33ac0bc5cb4a
EWanjau <33689244+EWanjau@users.noreply.github.com>
2022-03-19 00:24:55 +0300

    multiplacation

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * mul - multiplies two inputs
+ * @a: one value
+ * @b: 2nd value
+ * Return: the result of the operation
+ */
+int mul(int a, int b)
+{
+	int result;
+
+	result = a * b;
+
+	return (result);
+}
