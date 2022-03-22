f92310d61d789bc6ad27ee58e021b6a896b6ab0f
EWanjau <33689244+EWanjau@users.noreply.github.com>
2022-03-22 09:50:02 +0300

    function that changes the value of input to 98

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -2,8 +2,7 @@
 
 /**
  * reset_to_98 - resets a value of a variable using a pointer
- *@*n= is the pointer that changes the value of the variable
- *@n= the variable we are changing
+ *@n: the variable we are changing
  * Return: The value of the pointer
  */
 void reset_to_98(int *n)
