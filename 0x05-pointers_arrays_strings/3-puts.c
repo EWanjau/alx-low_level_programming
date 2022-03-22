8eede0b34b2d23df020095ee581b2f7aeaaf2a25
EWanjau <wamwanjau@gmail.com>
2022-03-22 05:13:18 -0700

    the function prints a string

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _puts - prints like stdout
+ * @str ; input string
+ * Return: Always 0 (Success)
+ */
+void _puts(char *str)
+{
+	int i;
+
+	i = 0;
+
+	while (str[i] != '\0')
+		_putchar(str[i]);
+
+	_putchar("\n");
+}
