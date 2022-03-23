78143af22102b062d2b9611fc907704eac9a4661
EWanjau <wamwanjau@gmail.com>
2022-03-23 04:56:34 -0700

    the function gets the last half of strings and prints it to stdout

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * puts_half - prints the last half of a string input
+ * @str: string input
+ * Return: Always 0
+ */
+
+void puts_half(char *str)
+{
+	int k, l;
+
+	k = 0;
+
+	while (str[k] != '\0')
+	{
+		k++;
+	}
+	if (k % 2 == 0)
+	{
+	for (l = (k / 2); l <= k; l++)
+		_putchar(str[l]);
+	}
+	else 
+	{
+		for (l = ((k - 1) / 2); l <= k; l++)
+			_putchar(str[l]);
+	}
+	_putchar('\n');
+}
