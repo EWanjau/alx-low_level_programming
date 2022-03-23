2bdce74edf12308f18677279eb64ae70fe161982
EWanjau <wamwanjau@gmail.com>
2022-03-23 05:17:54 -0700

    the function copes a string from a source to destination

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * *_strcpy - copies the string from a source to destination
+ * @ptr; destination string
+ * @s1: the source string to be copied
+ * Return: the pointer to the dstination
+ */
+char *_strcpy(char *dest, char *src)
+{
+
+}
