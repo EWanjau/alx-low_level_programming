3d21df7acda634a42ba16ce20f77d909c5a7c56e
EWanjau <33689244+EWanjau@users.noreply.github.com>
2022-03-24 12:55:09 +0300

    function copies strings

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -2,9 +2,8 @@
 
 /**
  * _strcpy - copies the string from a source to destination
- * @ptr: destination string
+ * @dest: destination string
  * @src: source string
- * @s1: the source string to be copied
  * Return: the pointer to the dstination
  */
 char *_strcpy(char *dest, char *src)
