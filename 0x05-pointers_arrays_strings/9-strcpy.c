6747c129155e90fdf93a622c52ae625309750499
EWanjau <33689244+EWanjau@users.noreply.github.com>
2022-03-24 12:41:07 +0300

    function copies a string to destination variable

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -1,8 +1,9 @@
 #include "main.h"
 
 /**
- * *_strcpy - copies the string from a source to destination
- * @ptr; destination string
+ * _strcpy - copies the string from a source to destination
+ * @ptr: destination string
+ * @src: source string
  * @s1: the source string to be copied
  * Return: the pointer to the dstination
  */
@@ -10,10 +11,10 @@ char *_strcpy(char *dest, char *src)
 {
 	int i, k;
 
-	for (i =0; src[i] != '\0'; i++)
+	for (i = 0; src[i] != '\0'; ++i)
 		dest[i] = src[i];
 	dest[i] = '\0';
-	for (k = 0; dest[i] != '\0'; k++)
+	for (k = 0; dest[i] != '\0'; ++k)
 		_putchar(dest[k]);
 	_putchar('\n');
 	
