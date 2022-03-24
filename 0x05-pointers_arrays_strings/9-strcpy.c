79ec4c98cb1c55079d9d44b804cc5f86552e8ce3
EWanjau <33689244+EWanjau@users.noreply.github.com>
2022-03-24 12:46:40 +0300

    function that copies string

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -9,13 +9,13 @@
  */
 char *_strcpy(char *dest, char *src)
 {
-	int i, k;
+	int i;
 
 	for (i = 0; src[i] != '\0'; ++i)
 		dest[i] = src[i];
+	
 	dest[i] = '\0';
-	for (k = 0; dest[i] != '\0'; ++k)
-		_putchar(dest[k]);
+	
 	_putchar('\n');
 	
 	return (dest);
