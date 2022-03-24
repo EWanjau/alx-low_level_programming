c4b92597839c5f2c764589a278ca4a4f07a385ee
EWanjau <33689244+EWanjau@users.noreply.github.com>
2022-03-24 15:56:14 +0300

    function the copies a string to the nth time

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -5,6 +5,7 @@
  * @n: times copiesd
  * @src: source file
  * @dest: destination file
+ * @ptr: stores the destination 
  * Return: destination fies
  */
 char *_strncpy(char *dest, char *src, int n)
