3224d429e051e574d454176d6324632573f3093b
EWanjau <wamwanjau@gmail.com>
2022-03-29 05:15:13 -0700

    find the substring in a bigger string

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _strstr - finds the substring in a string
+ * @haystack: the destination string
+ * @needle: substring
+ * Return: pointer to the substring
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	while (*haystack != '\0')
+	{
+		if (*haystack == *needle)
+			return (haystack);
+		haystack++;
+	}
+	return (NULL);
+}
