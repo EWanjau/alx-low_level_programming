b4b204a18d6c9c1c3dc1fa62564c9c4bc4a88e58
EWanjau <wamwanjau@gmail.com>
2022-03-29 03:09:21 -0700

    copy a string froma specific position

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _strpbrk - copies a string that natches another
+ * @s: destination string
+ * @accept: source string
+ * Return: the destination
+ */
+char *_strpbrk(char *s, char *accept)
+{
+	for (int i = 0; s[i] != '\0'; i++)
+	{
+		for (int j = 0; accept[j] != '\0'; j++)
+		{
+			if (s[i] == accept[j])
+				return (s);
+			s++;
+		}
+	}
+}
