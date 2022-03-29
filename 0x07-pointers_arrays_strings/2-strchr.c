d17b7b4b0af7ae879be678453d464deb61c4e3a6
EWanjau <wamwanjau@gmail.com>
2022-03-29 02:13:33 -0700

    function returning position of character

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _strchr - checks the position of a character and returnsthe string
+ * @s: the pointer ot the string
+ * @c: the character whose index we look for
+ * Return: s or null if the character is missing
+ */
+
+char *strchr(char *s, char c)
+{
+	while (*s != '\0')
+	{
+		if (*s == c)
+			return (s);
+		*s++;
+	}
+	return (NULL);
+}
