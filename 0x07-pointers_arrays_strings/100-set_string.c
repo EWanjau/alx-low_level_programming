5d79d0c3e9f478c647fdbec7b1121d906ddfbc82
EWanjau <wamwanjau@gmail.com>
2022-03-30 01:45:57 -0700

    set the pointer to character

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * set_string - sets a pointer to a character
+ * @s: the pointer tommemory location
+ * @to: the character 
+ * Return: the character
+ */
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
