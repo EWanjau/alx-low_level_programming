a4c4df7d7748d430446935ab26f1a05ba7082f1a
EWanjau <wamwanjau@gmail.com>
2022-03-30 08:15:17 -0700

    returns to a number raised to y

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _pow_recursion - multiplies by power of a number
+ * @x:the no to be multiplies
+ * @y:the power raised
+ * Return:the power of x
+ */
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+	{
+		return(-1);
+	}
+	if (y == 0)
+	{
+		return(1);
+	}
+	else
+	{
+		return(x * _pow_recursion(x, y - 1));
+	}
+}
