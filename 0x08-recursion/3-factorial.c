d3d3966488e6af0aa7006eb39ebe10204b221538
EWanjau <33689244+EWanjau@users.noreply.github.com>
2022-03-30 23:42:31 +0300

    get the factorial

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -7,7 +7,7 @@
  */
 int factorial(int n)
 {
-	if (n < 1)
+	if (n <= 1)
 	{
 		return (-1);
 	}
@@ -15,5 +15,6 @@ int factorial(int n)
 	{
 		return (1);
 	}
-	return (n * factorial(n - 1));
+	else
+		return (n * factorial(n - 1));
 }
