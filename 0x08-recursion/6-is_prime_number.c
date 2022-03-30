f92b9cbf4d867e26a787104e9b55d075b6378f7f
EWanjau <33689244+EWanjau@users.noreply.github.com>
2022-03-30 23:03:53 +0300

    check for the prime numbers

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -8,10 +8,10 @@ int prime(int n, int j);
 int is_prime_number(int n)
 {
 	if (n <= 1)
-		return(0);
+		return (0);
 	else if (prime(n, n / 2) > 0)
-		return(1);
-	return(0);
+		return (1);
+	return (0);
 }
 /**
  * prime - checks for the conditions of prime number
@@ -22,9 +22,9 @@ int is_prime_number(int n)
 int prime(int n, int j)
 {
 	if (j == 1)
-		return(1);
+		return (1);
 	if (n % j == 0)
-		return(0);
+		return (0);
 	else
 		return (prime(n, j - 1));
 }
