effbcd10b06df3705ec32100a70f1556ab9c462f
EWanjau <wamwanjau@gmail.com>
2022-03-30 09:15:50 -0700

    squareroot finder

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,30 @@
+#include "main.h"
+int squarert(int n, int j);
+/**
+ * _sqrt_recursion - condion chekcer
+ * @n: input
+ * Return:squareroot or -1
+ */
+int _sqrt_recursion(int n)
+{
+	if (n < 0)
+		return(-1);
+	else
+		return(squarert(n, (n + 1) / 2));
+}
+/**
+ * squarert: checks for natural numbers
+ * @n: input
+ * @j: counter
+ * Return: squareroot or -1
+ */
+int squarert(int n, int j)
+{
+	if (j < 1)
+		return (-1);
+	else if (j * j == n)
+		return(j);
+	else
+		return(squarert(n, j - 1));
+}
+
