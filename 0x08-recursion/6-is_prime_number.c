6b9f9b7dfabc850e738e216e7ad89351e493f503
EWanjau <wamwanjau@gmail.com>
2022-03-30 09:06:01 -0700

    checks for prime numbers

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * is_prime_number - checks for the prime number
+ * @n: input
+ * Return: 1 for prime number
+ */
+int prime(int n, int j);
+int is_prime_number(int n)
+{
+	if (n <= 1)
+		return(0);
+	else if (prime(n, n / 2) > 0)
+		return(1);
+	return(0);
+}
+/**
+ * prime - checks for the conditions of prime number
+ * @n: input
+ * @j: n / 2, checks if no is greater than 0
+ * Return: prime checker
+ */
+int prime(int n, int j)
+{
+	if (j == 1)
+		return(1);
+	if (n % j == 0)
+		return(0);
+	else
+		return (prime(n, j - 1));
+}
