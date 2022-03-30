d4811f3fad665ac9e05addd0779917d2625d1e30
EWanjau <33689244+EWanjau@users.noreply.github.com>
2022-03-30 23:15:11 +0300

    checks for an empty string

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ *is_palindrome - checks for palindromes in strings
+ *@s: input
+ *Return: 1 for palindrome and 0 if else
+ */
+int is_palindrome(char *s)
+{
+  if (*s == " ")
+    return (1);
+  else
+    return (-1);
+}
