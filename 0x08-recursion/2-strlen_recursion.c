706777ceb1c43210e43003415d87074d2cc223d2
EWanjau <wamwanjau@gmail.com>
2022-03-30 07:50:39 -0700

    counts characters in a string

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _strlen_recursion - ocunts the characters in a string
+ * @s: string input
+ * Return:string count
+ */
+int _strlen_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		return(0);
+	}
+	else
+	{
+		return(1 + _strlen_recursion(s + 1));
+	}
+}
