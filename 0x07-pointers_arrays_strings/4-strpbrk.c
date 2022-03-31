bb48f14009ebfce0898b6434db49f77787543682
EWanjau <33689244+EWanjau@users.noreply.github.com>
2022-03-31 11:44:18 +0300

    search a string for a set of bytes

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -1,5 +1,5 @@
 #include "main.h"
-
+#include "stdio.h"
 /**
  * _strpbrk - copies a string that natches another
  * @s: destination string
@@ -8,9 +8,12 @@
  */
 char *_strpbrk(char *s, char *accept)
 {
-	for (int i = 0; s[i] != '\0'; i++)
+	int i;
+	int j;
+	
+	for (i = 0; s[i] != '\0'; i++)
 	{
-		for (int j = 0; accept[j] != '\0'; j++)
+		for (j = 0; accept[j] != '\0'; j++)
 		{
 			if (s[i] == accept[j])
 				return (s);
