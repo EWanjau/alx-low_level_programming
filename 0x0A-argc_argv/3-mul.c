d767cc86ee7ea71424e81f9f79793b4c0222d123
EWanjau <wamwanjau@gmail.com>
2022-04-06 05:37:20 -0700

    multiply arguments

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -12,13 +12,17 @@ int main(int argc, char *argv[])
 	int i = 0;
 	int mult;
 
-	if (argc > 1)
+	if (argc == 3)
 	{
 		for (i = 1; i < argc; i++)
 		{
 			mult = atoi(argv[i]) * atoi(argv[i - 1]);
 		}
+		printf("%d\n", mult);
 	}
-	printf("%d\n", mult);
+	else
+	{
+		printf("Error\n");
+	}
 	return (0);
 }
