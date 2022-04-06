3bc18e0a2ce86d426954fcf4621359b1a3a23d80
EWanjau <wamwanjau@gmail.com>
2022-04-06 05:00:38 -0700

    no of arguments

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,12 +1,20 @@
 #include "main.h"
 #include "stdio.h"
+#include "stdlib.h"
 
 /**
  * main - prints the number of arguments
  * Return: tota arguments
  */
 int main(int argc, char* argv[])
-{i
-	printf("Arguments: %d\n", argc);
+{
+	int i = 0; 
+
+	while (i < argc)
+	{
+		printf("No of Arguments: %d, Name: %s\n", argc, argv[i]);
+		i++;
+	}
+
 	return (0);
 }
