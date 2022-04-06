4a9b2d1dae013235abbb6e8af6b009f3f32ccd64
EWanjau <wamwanjau@gmail.com>
2022-04-06 03:16:12 -0700

    name of program

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include "stdlib.h"
+
+/**
+ * whatsmyname - prints the name of a program
+ * Reurn: name
+ */
+int main(int argc, char* argv[])
+{
+	int i;
+
+	printf("%s\n",argv[0]);
+	return (0);
+}
