a86891bf5ec951fd94f648812542213b8659ff2f
EWanjau <33689244+EWanjau@users.noreply.github.com>
2022-04-09 00:19:17 +0300

    sized by malloc

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,20 @@
+#include "stdio.h"
+#include "stdlib.h"
+#include "limits.h"
+
+/**
+*malloc_checked - allocates memory space
+*@b: input
+*Return: size
+*/
+void *malloc_checked(unsigned int b)
+{
+    char *ptr;
+  
+    ptr = malloc(sizeof(b));
+    
+    if (ptr == NULL)
+      return (98);
+    
+    return (ptr);
+}
