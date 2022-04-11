2830115dbbfff253bd17b8a45b4ec6d7b30f2a16
EWanjau <wamwanjau@gmail.com>
2022-04-11 14:06:39 -0700

    first structures

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,19 @@
+#include "dog.h"
+
+/**
+ * init_dog - initializes a variable of structure dog
+ * @name: input
+ * @age:input
+ * @owner: input
+ * @d:input
+ * Return: 0 for success
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	struct dog my_dog;
+
+	mydog.d = &my_dog;
+	mydog.name = "Poppy";
+	mydog.age = 3.5;
+	mydog.owner = "Bob";
+}
