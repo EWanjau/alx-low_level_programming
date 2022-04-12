422ee92d8e514adcdeceb19e58e1ef83ef48c307
EWanjau <wamwanjau@gmail.com>
2022-04-12 07:17:24 -0700

    print struct dig

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -4,9 +4,16 @@
 /**
  * print_dog - prints the structure of the dog
  * @d: input
- * @Return: 0 for SUCCESS
  */
 void print_dog(struct dog *d)
 {
-	
+	if (d == NULL)
+		print("(nil)");
+
+	if (d->name == NULL)
+		d->name = "(nil)";
+	if (d->owner == NULL)
+		d->owner = "(nil)";
+
+	printf("Name: %s\nAge: %2.6f\nOwner: %s\n", d->name, d->age, d->owner);
 }
