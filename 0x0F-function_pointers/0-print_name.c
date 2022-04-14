d9ff035423b1824a41f7721b590e88c2f7cbe71c
EWanjau <wamwanjau@gmail.com>
2022-04-13 23:41:26 -0700

    print the name parsed

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,16 @@
+#include "stdio.h"
+#include "stdlib.h"
+#include "function_pointers.h"
+
+/**
+ * print_name - function pointer to print
+ * @name: input
+ * @f:input function pointer
+ * Return: 0 success
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	if (name == NULL || f == NULL)
+		return;
+	f(name);
+}
