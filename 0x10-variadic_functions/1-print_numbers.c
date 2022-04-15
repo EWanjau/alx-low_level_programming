bf333e1c421fc568d61c009d2085023ea1c19889
EWanjau <wamwanjau@gmail.com>
2022-04-15 07:44:54 -0700

    all numbers printed

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,24 @@
+#include "variadic_functions.h"
+
+/**
+ * print_numbers - prints all numbers parsed
+ * @separator: character spacing the numbers
+ * @n: number of arguments
+ * Return: void
+ */
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	va_list ap;
+	unsigned int i;
+
+	va_start(ap, n);
+	if (separator == NULL)
+		return;
+	for (i = 0; i < n; i++)
+	{
+		printf("%d", va_arg(ap, int));
+		if (i != (n - 1))
+			printf("%s", separator);
+	}
+	printf("\n");
+}
