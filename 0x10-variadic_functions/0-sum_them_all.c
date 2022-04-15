8863beeb7bc742413c83549967a86ec6834ec782
EWanjau <wamwanjau@gmail.com>
2022-04-15 07:15:26 -0700

    add them

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -14,10 +14,10 @@ int sum_them_all(const unsigned int n, ...)
 	va_start(ap, n);
 
 	if (n == 0)
-		return(0);
+		return (0);
 	sum = 0;
 	for (i = 0; i < n; i++)
 		sum = sum + va_arg(ap, int);
 	va_end(ap);
-	return(sum);
+	return (sum);
 }
