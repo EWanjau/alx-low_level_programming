7b690032bbbed52b067d1806f5077e2f1bcf09bf
EWanjau <33689244+EWanjau@users.noreply.github.com>
2022-04-15 13:17:24 +0300

    Create 3-_op_functions.c

---
+++ 0x0F-function_pointers/3-_op_functions.c
@@ -0,0 +1,65 @@
+#include "3-calc.h"
+#include "stdio.h"
+/**
+* op_add - returns sum
+ * @a: input
+ * @b:input
+ * Return: sum
+*/
+int op_add(int a, int b)
+{
+    return (a + b);
+}
+
+/**
+*op_sub - difference of two numbers
+ * @a: input
+ * @b: input
+ * Return: difference
+*/
+int op_sub(int a, int b)
+{
+    return (a - b);
+}
+
+/**
+* op_mul - multiplacation of two numbers
+ * @a: input
+ * @b: input
+ * Return: multiplacation
+*/
+int op_mul(int a, int b)
+{
+    return (a * b);
+}
+
+/**
+* op_div - division of two numbers
+ * @a: input
+ * @b: input
+ * Return: division
+*/
+int op_div(int a, int b)
+{
+    if (b == 0) {
+        printf("Error");
+        return (1);
+    }
+    return (a / b);
+}
+
+/**
+* op_mod - remainder of two numbers
+ * @a: input
+ * @b: input
+ * Return: remainder
+*/
+int op_mod(int a, int b)
+{
+    if (b == 0)
+    {
+        printf("Error");
+        return (1);
+    }
+    return (a % b);
+}
