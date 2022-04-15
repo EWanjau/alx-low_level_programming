6ce56bec6dfcf0c30a0b5e399448081db89f6720
EWanjau <33689244+EWanjau@users.noreply.github.com>
2022-04-15 13:39:29 +0300

    Update 3-main.c

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -8,7 +8,7 @@
  * @argv: argument name/list
  * Return: void
  */
-void main(int argc, char *argv[])
+int main(int argc, char *argv[])
 {
 	char op;
 	int num1, num2, result;
@@ -26,16 +26,16 @@ void main(int argc, char *argv[])
 	}
 	
 	op = *argv[2];
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[3]);
 	
-	if ((op == '/' || op == '%') && (argv[3] == '0'))
+	if ((op == '/' || op == '%') && (num2 == '0'))
 	{
 		printf("Error\n");
 		exit(100);
 	}
 	
 	func = get_op_func(argv[2]); 
-	num1 = atoi(argv[1]);
-	num2 = atoi(argv[3]);
 	
 	result = func(num1, num2);
 	printf("%d\n", result);
