36aa0863343171aefe576f8069112b2a70a48777
EWanjau <wamwanjau@gmail.com>
2022-04-15 03:46:57 -0700

    update

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -24,21 +24,18 @@ int main(int argc, char *argv[])
 		printf("Error\n");
 		exit(99);
 	}
-	
+
 	op = *argv[2];
 	num1 = atoi(argv[1]);
 	num2 = atoi(argv[3]);
-	
 	if ((op == '/' || op == '%') && (num2 == '0'))
 	{
 		printf("Error\n");
 		exit(100);
 	}
-	
-	func = get_op_func(argv[2]); 
-	
+	func = get_op_func(argv[2]);
 	result = func(num1, num2);
 	printf("%d\n", result);
-	
+
 	return (0);
 }
