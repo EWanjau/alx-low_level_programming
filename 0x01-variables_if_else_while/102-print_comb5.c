5397e95d6207c086ba1fc6c0d487e6114b555347
EWanjau <33689244+EWanjau@users.noreply.github.com>
2022-04-16 17:55:08 +0300

    two set of digits

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,33 @@
+#include "stdio.h"
+
+/**
+ * main - prints possible combinations of two-two-digit numbers
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+    int n, x;
+
+    for (n = 0; n < 100; n++)
+    {
+        for (x = 0; x < 100; x++)
+        {
+            if (n < x)
+            {
+                putchar((n / 10) + 48);
+                putchar((n % 10) + 48);
+                putchar(' ');
+                putchar((x / 10) + 48);
+                putchar((x % 10) + 48);
+                if (n != 98 || x != 99)
+                {
+                    putchar(',');
+                    putchar(' ');
+                }
+            }
+
+        }
+    }
+    putchar('\n');
+    return (0);
+}
