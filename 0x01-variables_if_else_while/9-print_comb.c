973cf2f8e73de0d0a8ce5f34a7340827e7066f58
EWanjau <wamwanjau@gmail.com>
2022-04-16 07:00:48 -0700

    correct code

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,14 +1,24 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include "stdio.h"
+#include "stdlib.h"
 
-/** 
+/**
  * main - Entry Point
- * 
+ *
  * Return: Always 0 (Success)
  */
+int main(void)
+{
+	int x;
 
- int main(void){
-	
-
+	for (x = 48; x < 58; x++)
+	{
+		putchar(x);
+		if (x != 57)
+		{
+			putchar(',');
+			putchar(',');
+		}
+	}
+	putchar('\n');
 	return (0);
- }
+}
