d28846237592dad3fe6f2053dce9a9528c55ac6f
EWanjau <wamwanjau@gmail.com>
2022-04-16 06:37:39 -0700

    code corection

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,16 +1,17 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include "stdio.h"
+#include "stdlib.h"
 
 /**
 *main - Entry point
 *
 *Return: Always 0 (Success)
 */
-
 int main(void)
 {
 	int num;
-	for(num=0;num<10;num++)
+
+	for (num = '0'; num <= '9'; num++)
 		putchar(num);
-	return(0);
+	putchar('\n');
+	return (0);
 }
