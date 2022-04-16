90880c2483e0e0a5beaf9815d2df08e3ddef7689
EWanjau <wamwanjau@gmail.com>
2022-04-16 06:04:00 -0700

    betty corrections

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,18 +1,19 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include "stdio.h"
+#include "stdlib.h"
 
 /**
  * main - Entry point
- * 
+ *
  * Return: Always 0 (Success)
  */
+int main(void)
+{
+	char ch;
 
- int main(void){
-	char ch; 
-	for(ch='a';ch<='z';ch++)
+	for (ch = 'a'; ch <= 'z'; ch++)
 		putchar(ch);
-	for(ch='A';ch<='Z';ch++)
+	for (ch = 'A'; ch <= 'Z'; ch++)
 		putchar(ch);
 	putchar('\n');
 	return (0);
- }
+}
