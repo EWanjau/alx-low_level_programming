171152870dfc60d643f7759bf3675d37e27c6e04
EWanjau <wamwanjau@gmail.com>
2022-04-21 16:15:16 -0700

    no of elements

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * list_len - shows number of elements
+ * @h: pointer to head
+ * Return: no of elements
+ */
+size_t list_len(const list_t *h)
+{
+	size_t count = 0;
+
+	if (h == NULL)
+		printf("Error\n");
+	while (h != NULL)
+	{
+		count++;
+		h = h->next;
+	}
+	return (count);
+}
