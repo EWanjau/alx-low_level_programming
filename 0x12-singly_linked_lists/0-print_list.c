27c8bffb7e577b39880a52d7b5fba53d12b3a8e1
EWanjau <wamwanjau@gmail.com>
2022-04-21 16:03:04 -0700

    update

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -3,7 +3,6 @@
 
 /**
  * print_list - prints elemeents of a linked list
- * @list_t: input list
  * @h: pointer to head
  * Return: count
  */
@@ -20,7 +19,7 @@ size_t print_list(const list_t *h)
 		count++;
 		if (h->str == NULL)
 			printf("[%d] %s\n", 0, "(nil)");
-		else 
+		else
 			printf("[%d] %s\n", h->len, h->str);
 		h = h->next;
 	}
