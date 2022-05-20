2dff199047abae85e404be79b9b93ded7b8d0b38
EWanjau <wamwanjau@gmail.com>
2022-05-20 16:46:55 +0000

    fizzbuzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -13,22 +13,19 @@ int main(void)
 
 	for (i = 1; i <= 100; i++)
 	{
-		if (i % 3 == 0 && i % 5 == 0)
+		if (i % 15 == 0)
 		{
-			printf(" FizzBUZZ");
+			printf(" FizzBUZZ\t");
 		} else if (i % 5 == 0)
 		{
-			printf(" Buzz");
+			printf(" Buzz\t");
 		} else if (i % 3 == 0)
 		{
-			printf(" Fizz");
-		} else if (i == 1)
-		{
-			printf(" %d", i);
+			printf(" Fizz\t");
 		}
 		else
 		{
-			printf(" %d", i);
+			printf(" %d\t", i);
 		}
 	}
 	printf("\n");
