54988edb23595b2780845d46d412652340892fd9
EWanjau <wamwanjau@gmail.com>
2022-05-25 20:05:30 +0000

    copy

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -9,13 +9,23 @@
 unsigned int _strspn(char *s, char *accept)
 {
 	int count = 0;
-	int i;
-	int j = 0;
+	int i, j, f;
 
 	for (i = 0; s[i] != '\0'; i++)
 	{
-		if (s[i] == accept[j])
-			count++;
+		f = 0;
+		for (j = 0; accept[j] != '0'; j++)
+		{
+			if (s[i] == accept[j])
+			{
+				count++;
+				f = 1;
+			}
+		}
+		if (f == 0)
+		{
+			return (count);
+		}
 	}
-	return (count);
+	return (0);
 }
