f93dce2bca9641a3a2eb2c8458ace7520a3ea863
EWanjau <wamwanjau@gmail.com>
2022-05-25 20:11:50 +0000

    copy

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -8,14 +8,13 @@
  */
 char *_strpbrk(char *s, char *accept)
 {
-	int i;
-	int j;
+	int i, j;
 	
-	for (i = 0; s[i] != '\0'; i++)
+	for (i = 0; *s != '\0'; i++)
 	{
 		for (j = 0; accept[j] != '\0'; j++)
 		{
-			if (s[i] == accept[j])
+			if (*s == accept[j])
 				return (s);
 		}
 		s++;
