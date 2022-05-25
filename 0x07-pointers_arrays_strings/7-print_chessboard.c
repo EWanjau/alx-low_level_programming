56b2b5cfaae786ee6ca577d940a83c34dca01d57
EWanjau <wamwanjau@gmail.com>
2022-05-25 20:42:53 +0000

    chessboard

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -2,15 +2,14 @@
 
 /**
  * print_chessboard - orints the layout of a chessboard
- * @a:the rows of the matrix
- * @8: the number of columns
+ * @a:the rows of the matrix and the number of columns
  * Return: the matrix
  */
 void print_chessboard(char (*a)[8])
 {
 	int i, j;
-	
-	for (i = 0; i < *a[i]; i++)
+
+	for (i = 0; i < 8; i++)
 	{
 		for (j = 0; j < 8; j++)
 			_putchar(a[i][j]);
