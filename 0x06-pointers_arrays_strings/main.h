3b1e8308a5c84fe69989f92bfbc5e14123356c48
EWanjau <wamwanjau@gmail.com>
2022-05-25 08:47:16 +0000

    add another func_prototype

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,4 +1,3 @@
-int _putchar(char c);
 char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
 char *_strncpy(char *dest, char *src, int n);
@@ -7,5 +6,9 @@ void reverse_array(int *a, int n);
 char *string_toupper(char *);
 char *cap_string(char *);
 char *leet(char *);
-
-	
+#include "stdio.h"
+#include "stdlib.h"
+#include "unistd.h"
+#include "string.h"
+int _putchar(char c);
+void print_number(int n);
