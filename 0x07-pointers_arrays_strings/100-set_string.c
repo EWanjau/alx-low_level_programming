6bad56e153e67181fff67152b48ba1f5e29105bb
EWanjau <wamwanjau@gmail.com>
2022-05-26 06:40:40 +0000

    reset to character string

--- 0x07-pointers_arrays_strings/100-set_string.c
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -3,7 +3,7 @@
 /**
  * set_string - sets a pointer to a character
  * @s: the pointer tommemory location
- * @to: the character 
+ * @to: the character
  * Return: the character
  */
 void set_string(char **s, char *to)
