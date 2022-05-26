ecf5aab0d9a01bd3adf6323f91063be544e80572
EWanjau <wamwanjau@gmail.com>
2022-05-26 18:19:18 +0000

    correct str concat

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,41 +1,45 @@
 #include "main.h"
-#include "stdlib.h"
-#include "stdio.h"
 
 /**
- * str_concat - add two stdings
+ * str_concat - concatenate two strings
  * @s1: 1st str
  * @s2: 2nd str
- * Return: concatenated str
+ * Return: pointer to the new concatenated str
  */
 char *str_concat(char *s1, char *s2)
 {
-	int size1, size2, i = 0;
+	unsigned int size1 = 0, size2 = 0, i = 0, j = 0;
 	char *arr;
 
-	if (s1 == NULL && s2 == NULL)
-		s1 = s2 = "";
-	for (size1 = 0; size1 <= *s1; size1++)
-	{
-	}
-	for (size2 = 0; size2 <= *s2; size2++)
-	{
-	}
+	while (s1 && s1[size1])
+		size1++;
+
+	while (s2 && s2[size2])
+		size2++;
+
 	arr = malloc(sizeof(char) * (size1 + size2 + 1));
 
 	if (arr == NULL)
 		return (NULL);
-	while (*s1)
+
+	if (s1)
 	{
-		arr[i] = *s1;
-		i++;
-		s1++;
+		while (i < size1)
+		{
+			arr[i] = s1[i];
+			i++;
+		}
 	}
-	while (*s2)
+	if (s2)
 	{
-		arr[i] = *s2;
-		i++;
-		s2++;
+		while (i < (size1 + size2))
+		{
+			arr[i] = s2[j];
+			i++;
+			j++;
+		}
 	}
+	arr[i] = '\0';
+
 	return (arr);
 }
