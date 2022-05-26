d8abf0c0d0b63202f4b74e0f0b1faec7e23563c0
EWanjau <wamwanjau@gmail.com>
2022-05-26 17:37:08 +0000

    update putchar

--- 0x09-static_libraries/cfiles/_putchar.c
+++ 0x09-static_libraries/cfiles/_putchar.c
@@ -1,22 +1,11 @@
-#include "main.h"
+#include "unistd.h"
 
 /**
- * main - Print a string
- *
- * Return: Always 0 (Success)
+ * _putchar - dispays character to stdout
+ * @c: the character to output
+ * Return: 1 on success
  */
-
-int main(void)
+int _putchar(char c)
 {
-	_putchar('_');
-	_putchar('p');
-	_putchar('u');
-	_putchar('t');
-	_putchar('c');
-	_putchar('h');
-	_putchar('a');
-	_putchar('r');
-	_putchar('\n');
-
-	return (0);
+	return(write(1, &c, 1));
 }
