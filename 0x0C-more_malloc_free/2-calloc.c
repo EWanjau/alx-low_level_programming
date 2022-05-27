f6f0b41daa6fd6f61af88f17853ffd55fdd43f82
EWanjau <wamwanjau@gmail.com>
2022-05-27 10:18:27 +0000

    update memory allocater

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,40 @@
+#include "main.h"
+
+/**
+ *_memset - sets memory to a value
+ *@b: the bytes to set memory to
+ *@s: the pointer to array passed
+ *@n: the size of array
+ *Return: pointer to the set memory array
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+	{
+		s[i] = b;
+	}
+	return (s);
+}
+/**
+ *_calloc - allocates memory for an array
+ *@nmemb: no of elements in array
+ *@size: no of bytes
+ *Return: pointer to allocated memory space
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	char *ptr;
+
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+
+	ptr = malloc(size * nmemb);
+
+	if (ptr == NULL)
+		return (NULL);
+	_memset(ptr, 0, nmemb * size);
+
+	return (ptr);
+}
