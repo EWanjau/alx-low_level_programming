08211b919a36555d97a58ffcf186035c82ee3eef
EWanjau <wamwanjau@gmail.com>
2022-05-29 02:54:46 +0000

    update

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -20,9 +20,9 @@ int (*get_op_func(char *s))(int, int)
 
 	while (ops[i].op)
 	{
-		if (*ops[i].op == *s)
+		if (strcmp(ops[i].op, s) == 0)
 		{
-		return (ops[i].f);
+			return (ops[i].f);
 		}
 	i++;
 	}
