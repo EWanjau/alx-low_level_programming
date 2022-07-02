7b78962c070478988cbe72ad4127b03a0243cbfa
EWanjau <wamwanjau@gmail.com>
2022-07-02 08:45:13 +0000

    update

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -23,7 +23,7 @@ int append_text_to_file(const char *filename, char *text_content)
 		while (text_content[len])
 			len++;
 		if (write(fd, text_content, len) == -1)
-			return (-1)
+			return (-1);
 	}
 	close(fd);
 	return (1);
