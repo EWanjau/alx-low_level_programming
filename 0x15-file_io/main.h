285c407f2602f5e85b81730ca673638af1b3d168
EWanjau <wamwanjau@gmail.com>
2022-07-02 09:35:10 +0000

    update

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -13,5 +13,7 @@ ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
 int cp_file1_file2(const char *file_from, const char *file_to);
+int _close(int fd);
+int _cp(char *file_from, char *file_to);
 
 #endif
