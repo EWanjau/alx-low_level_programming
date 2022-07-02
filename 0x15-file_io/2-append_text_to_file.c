0cf6a600adba6064fda48c0c93ef0683d530f2ad
EWanjau <wamwanjau@gmail.com>
2022-07-02 08:37:13 +0000

    update

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -8,12 +8,13 @@
  */
 int append_text_to_file(const char *filename, char *text_content)
 {
-	int fd, len, by = 0;
+	int fd;
+	unsigned int len = 0;
 
 	if (!filename)
 		return (-1);
 
-	fd = open(filename, O_RDWR | O_APPEND);
+	fd = open(filename, O_WRONLY | O_APPEND);
 	if (fd < 0)
 		return (-1);
 
@@ -21,9 +22,8 @@ int append_text_to_file(const char *filename, char *text_content)
 	{
 		while (text_content[len])
 			len++;
-		by = write(fd, text_content, len);
-		if (by != len)
-			return (-1);
+		if (write(fd, text_content, len) == -1)
+		return (-1)
 	}
 	close(fd);
 
