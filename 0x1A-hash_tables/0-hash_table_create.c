e23a3b1b35467c8f485353a8dd5405bcb2007485
EWanjau <wamwanjau@gmail.com>
2022-09-23 17:22:50 +0300

    created a hash table and put into it data

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -1,3 +1,26 @@
-#include "hash_table.h"
+#include "hash.h"
 
+/**
+ * hash_table_create - creates a hash table
+ * @size: size of the array
+ * Return: a pointer to the hash table
+ */
 
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	hash_table_t *head;
+
+	if (size == 0)
+		return (NULL);
+
+	head = malloc(sizeof(hash_table_t));
+	if (!head)
+		return (NULL);
+
+	head->size = size;
+	head->array = calloc((size_t)head->size, sizeof(hash_node_t *));
+	if (head->array == NULL)
+		return (NULL);
+
+	return (head);
+}
