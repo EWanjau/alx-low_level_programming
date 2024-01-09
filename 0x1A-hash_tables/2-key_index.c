1348b495285485d8debc855cdc287451d222c4e4
EWanjau <wamwanjau@gmail.com>
2024-01-09 12:41:08 +0300

    done the hashtables

---
+++ 0x1A-hash_tables/2-key_index.c
@@ -0,0 +1,22 @@
+#include "hash_tables.h"
+
+/**
+ * key_index - returns the index from a hash function
+ * @key: the key
+ * @size: the size of array of the hash table
+ * 
+ * Return: index where the key/value pair is stored
+ */
+
+unsigned long int key_index(const unsigned char *key, unsigned long int size)
+{
+	unsigned long int hash;
+
+	if (size == 0)
+		return (0);
+
+	hash = hash_djb2(key);
+
+	return (hash % size);
+
+}
---
+++ 0x1A-hash_tables/README.md
@@ -0,0 +1,8 @@
+#0x1A. C - Hash tables
+---
+**Concepts**
+
+*Key Concept:*
+- Algorithms 
+- Data Structures in C
+- 
